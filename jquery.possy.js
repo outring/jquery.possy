f0b235982d9d01cc2febd9496082c98cfffd8bcf
artyv <outring@gmail.com>
2012-08-28 13:26:58 +0600

    Using base property parsing and value translation only when needed

--- jquery.possy.js
+++ jquery.possy.js
@@ -1,55 +1,62 @@
-(function ($) {
+(function ($, undefined) {
 
-    var support = (function () {
-        var style = document.createElement('div').style;
-        style.backgroundPosition = 'right top';
+	var support = (function () {
+		var style = document.createElement('div').style;
+		style.backgroundPosition = 'right top';
 
-        var result = {};
+		var result = { };
 
-        /** Check backgroundPositionX/backgroundPositionY support */
-        result.directions = 'backgroundPositionX' in style && 'backgroundPositionY' in style;
+		/** Check backgroundPositionX/backgroundPositionY support */
+		result.directions = 'backgroundPositionX' in style && 'backgroundPositionY' in style;
 
-        /** Check if top/right/bottom/left values translated to numeric values by browser */
-        result.translation = result.directions && style.backgroundPositionX.toLocaleLowerCase() !== 'right';
+		/** Check if top/right/bottom/left values translated to numeric values by browser */
+		result.translation = result.directions && style.backgroundPositionX.toLocaleLowerCase() !== 'right';
 
-        return result;
-    })();
+		return result;
+	})();
 
-    if (support.directions && support.translation)
-        return;
+	if (support.directions && support.translation)
+		return;
 
-    function parseValue(position) {
-        return position
-            .replace(/left|top/i, '0px')
-            .replace(/right|bottom/i, '100%')
-            .replace(/([0-9\.]+)(\s|$)/, "$1px$2");
-    }
+	function parseValue(position) {
+		return position.replace(/left|top/i, '0px').replace(/right|bottom/i, '100%');
+	}
 
-    function parsePosition(position) {
-        var parts = position.split(' ');
-        return {
-            x: parseValue(parts[0]),
-            y: parseValue(parts[1])
-        };
-    }
+	function getPositions(element) {
+		var parts = $.css(element, baseProperty).split(' ');
+		return { X: parts[0], Y: parts[1] };
+	}
 
-    var baseProperty = 'backgroundPosition';
+	var baseProperty = 'backgroundPosition';
 
-    $.each(['X', 'Y'], function (_, direction) {
-        var currentProperty = baseProperty + direction;
-        $.cssHooks[currentProperty] = {
-            set: function (element, value) {
-                var parts = parsePosition($.curCSS(element, baseProperty));
-                parts[direction.toLowerCase()] = parseValue(value);
-                element.style.backgroundPosition = parts.x + ' ' + parts.y;
-            },
-            get: function (element) {
-                return parsePosition($.curCSS(element, baseProperty))[direction.toLowerCase()];
-            }
-        };
-        $.fx.step[currentProperty] = function (fx) {
-            $.style(fx.elem, baseProperty + direction, fx.now + fx.unit);
-        };
-    });
+	$.each(['X', 'Y'], function (_, direction) {
+		var currentProperty = baseProperty + direction;
+		var hook;
+		if (support.directions)
+			hook = {
+				get: function (element, computed, isInternalCall) {
+					if (isInternalCall)
+						return undefined;
+					var value = $.css(element, currentProperty, /** isInternalCall */ true);
+					return computed ? parseValue(value) : value;
+				}
+			};
+		else
+			hook = {
+				get: function (element, computed) {
+					var value = getPositions(element)[direction];
+					return computed ? parseValue(value) : value;
+				},
+				set: function (element, value) {
+					var positions = getPositions(element);
+					positions[direction] = value;
+					element.style.backgroundPosition = positions.X + ' ' + positions.Y;
+				}
+			};
+		$.cssHooks[currentProperty] = hook;
+		$.fx.step[currentProperty] = function (fx) {
+			$.style(fx.elem, baseProperty + direction, fx.now + fx.unit);
+		};
+	});
 
 })(jQuery);
